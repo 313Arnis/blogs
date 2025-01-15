18c6de64c1b1bcf747d1c9c5ea5aaaa2bae26370
ipa23.a.razuvanovs <ipa23.a.razuvanovs@pjvt.local>
2025-01-15 09:39:32 +0200

    Createblog

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -1,6 +1,25 @@
 <?php
+$pageTitle = "Pievienot ierakstu";
 
-$sql = "INSERT * INTO posts";
-$params = ["content" => $_POST["content"]];
-header("Location: /"); exit();
-require "views/posts/create.views.php";
+
+
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['content'])) {
+        $content = $_POST['content'];  
+
+
+        $sql = "INSERT INTO posts (content) VALUES (:content)";
+        $params = ["content" => $content];
+
+        
+        $db->query($sql, $params);
+
+     
+        header("Location: /");
+        exit();
+    }
+}
+
+require "views/posts/create.views.php"; 
+?>
--- routes.php
+++ routes.php
@@ -3,8 +3,8 @@
 
 return [  
  "/"=>"controllers/posts/index.php",
+ "/about" => "controllers/story.php",
+ "/categories" => "controllers/categories.php",
  "/create" => "controllers/posts/create.php",
  "/show" => "controllers/posts/show.php",
- "/about" => "controllers/story.php",
- "/categories" => "controllers/categories.php"
 ];
