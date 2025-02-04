7fadbedf31f11c764ecd000ebe0c1c5a052c4b08
ipa23.a.razuvanovs <ipa23.a.razuvanovs@pjvt.local>
2025-02-04 13:44:41 +0200

    ss

--- views/components/header.php
+++ views/components/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $pageTitle ?? "uhoh" ?> </title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="http://localhost/css/styles.css">
 </head>
 <body>
