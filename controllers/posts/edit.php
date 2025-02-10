38c8af191f8dc3796cb0694d8e91c924a20808f4
ipa23.a.razuvanovs <ipa23.a.razuvanovs@pjvt.local>
2025-02-10 15:14:49 +0200

    Kategorijas

--- controllers/posts/edit.php
+++ controllers/posts/edit.php
@@ -2,33 +2,45 @@
 
 require "Validator.php";
 
+
 $sql = "SELECT * FROM posts WHERE id = :id";
-$params = ["id" => $_GET ["id"]];
+$params = ["id" => $_GET["id"]];
 $post = $db->query($sql, $params)->fetch();
+
+
+$sql = "SELECT * FROM categories";
+$params = [];
+$categories = $db->query($sql, $params)->fetchAll();
 
 
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
-    if(!Validator::string($_POST["content"],max: 50)){
+    $content = $_POST['content'];  
+    $category_id = $_POST['id']; 
+
+
+    if (!Validator::string($content, max: 50)) {
         $errors["content"] = "Saturam jābūt ievadītam, bet ne garākam par 50 rakstzīmēm";
     }
-  
+
+   
     if (empty($errors)) {
-        $content = $_POST['content'];  
+        $sql = "UPDATE posts SET content = :content, category_id = :category_id WHERE id = :id";
 
+        $params = [
+            "content" => $content,
+            "category_id" => $category_id,
+            "id" => $_GET["id"]  
+        ];
 
-        $sql = "UPDATE posts SET content = :content WHERE id = :id";
-        $params["content"] = $_POST["content"];
-
-        
+    
         $db->query($sql, $params);
 
-     
-        header("Location: /show?id=" . $_POST["id"]);
+        
+        header("Location: /show?id=" . $_GET["id"]);
         exit();
     }
 }
 
-
-
-require "views/posts/edit.views.php"; 
+require "views/posts/edit.views.php";
+?>
--- views/posts/edit.views.php
+++ views/posts/edit.views.php
@@ -7,4 +7,13 @@ require "views/components/navbar.php";
 <input  type="hidden" name="id" value="<?= $post['id']; ?>">
 <input  name="content" value="<?= $post['content'] ?? ''; ?>">
 <button>EDIT</button>
+<label for="category">Izvēlies kategoriju:</label>
+    <select  name="id">
+           <?php foreach ($categories as $category): ?>
+            <option name = "id" value= "<?= $category['id'] ?> ">
+            <?= $category['category_name'] ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+
 </form>
