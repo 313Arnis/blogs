d1dbcda0d273b9c553d64244cc384152c3d68ce0
ipa23.a.razuvanovs <ipa23.a.razuvanovs@pjvt.local>
2025-02-11 11:26:03 +0200

    Rediget edit kategoriju + bez katgegorijas

--- views/posts/show.view.php
+++ views/posts/show.view.php
@@ -1,16 +1,12 @@
 <?php require "views/components/header.php" ?>
 <?php require "views/components/navbar.php" ?>
 
-<h1><?= htmlspecialchars($post["content"]) ?> <br> Kategorija: <?= $post["category_name"] ?></h1>
+<h1><?= htmlspecialchars($post["content"]) ?> <br> Kategorija: <?= !empty($post["category_name"]) ? htmlspecialchars($post["category_name"]) : 'Nav kategorijas' ?></h1>
 
 <a href="/edit?id=<?= $post['id']; ?>">Rediģēt šo ierakstu</a>
 
-
 <form method="POST" action="/delete">
-
     <input type="hidden" name="id" value="<?= $post['id']; ?>">
-
-   
     <button >Izdzēst</button>
 </form>
 
